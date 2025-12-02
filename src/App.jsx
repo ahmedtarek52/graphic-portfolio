be937a68668e59e73c28a7bc9b5ff6c46e853689
ahmedtarek52 <117595019+ahmedtarek52@users.noreply.github.com>
2025-12-03 00:05:11 +0200

    remove import of app.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import AppRoutes from "./routes/AppRoutes";
 import useScrollToTop from "./hooks/useScrollToTop";
-import "./app.css";
 
 
 export default function App() {
