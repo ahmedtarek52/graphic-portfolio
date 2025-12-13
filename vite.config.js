8a462c490c48026ea10bda5e0067bcf0393e3519
ahmedtarek52 <117595019+ahmedtarek52@users.noreply.github.com>
2025-12-13 19:38:37 +0200

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(),react()],
-  build: {
-    sourcemap: false,
-    chunkSizeWarningLimit: 600,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          firebase: ['firebase/app', 'firebase/firestore', 'firebase/auth'],
-        }
-      }
-    }
-  }
 })
